dbe54581f59b9ab0948044be4ff84d606089f148
Nikita Lazarev <124895405+Lazarev-nikita@users.noreply.github.com>
2025-10-08 00:08:27 +0300

    Update sample_prime_numbers.cpp

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -1,18 +1,19 @@
-﻿// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// sample_prime_numbers.cpp - Copyright (c) Гергель В.П. 20.08.2000
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
-// Тестирование битового поля и множества
+#if defined(_MSC_VER)
+#  pragma execution_character_set("utf-8")
+#endif
 
+#include <iostream>
 #include <iomanip>
+#include <clocale>
 
-// #define USE_SET // Использовать класс TSet,
-                // закоментировать, чтобы использовать битовое поле
-
-#ifndef USE_SET // Использовать класс TBitField
+#ifndef USE_SET
 
 #include "tbitfield.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
 
 int main()
 {
@@ -22,19 +23,19 @@ int main()
   cout << "Тестирование программ поддержки битового поля" << endl;
   cout << "             Решето Эратосфена" << endl;
   cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
+  if (!(cin >> n) || n < 2) {
+    cout << "N должно быть >= 2" << endl;
+    return 0;
+  }
+
   TBitField s(n + 1);
-  // заполнение множества
   for (m = 2; m <= n; m++)
     s.SetBit(m);
-  // проверка до sqrt(n) и удаление кратных
   for (m = 2; m * m <= n; m++)
-    // если m в s, удаление кратных
     if (s.GetBit(m))
       for (k = 2 * m; k <= n; k += m)
         if (s.GetBit(k))
           s.ClrBit(k);
-  // оставшиеся в s элементы - простые числа
   cout << endl << "Печать множества некратных чисел" << endl << s << endl;
   cout << endl << "Печать простых чисел" << endl;
   count = 0;
@@ -49,10 +50,18 @@ int main()
     }
   cout << endl;
   cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+  return 0;
 }
+
 #else
 
 #include "tset.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
 
 int main()
 {
@@ -62,19 +71,19 @@ int main()
   cout << "Тестирование программ поддержки множества" << endl;
   cout << "              Решето Эратосфена" << endl;
   cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
+  if (!(cin >> n) || n < 2) {
+    cout << "N должно быть >= 2" << endl;
+    return 0;
+  }
+
   TSet s(n + 1);
-  // заполнение множества
   for (m = 2; m <= n; m++)
     s.InsElem(m);
-  // проверка до sqrt(n) и удаление кратных
   for (m = 2; m * m <= n; m++)
-    // если м в s, удаление кратных
     if (s.IsMember(m))
       for (k = 2 * m; k <= n; k += m)
-       if (s.IsMember(k))
-         s.DelElem(k);
-  // оставшиеся в s элементы - простые числа
+        if (s.IsMember(k))
+          s.DelElem(k);
   cout << endl << "Печать множества некратных чисел" << endl << s << endl;
   cout << endl << "Печать простых чисел" << endl;
   count = 0;
@@ -89,6 +98,8 @@ int main()
     }
   cout << endl;
   cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+  return 0;
 }
 
 #endif
